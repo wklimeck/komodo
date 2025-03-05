3fad023c66b08fb3bc47908dec1f178988c5412d
mbecker20 <becker.maxh@gmail.com>
2025-03-04 18:07:28 -0800

    update most deps

--- lib/logger/src/otel.rs
+++ lib/logger/src/otel.rs
@@ -6,35 +6,32 @@ use opentelemetry_sdk::{
   trace::{Sampler, Tracer},
   Resource,
 };
-use opentelemetry_semantic_conventions::{
-  resource::{SERVICE_NAME, SERVICE_VERSION},
-  SCHEMA_URL,
-};
+use opentelemetry_semantic_conventions::resource::SERVICE_VERSION;
 
 fn resource(service_name: String) -> Resource {
-  Resource::from_schema_url(
-    [
-      KeyValue::new(SERVICE_NAME, service_name),
-      KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
-    ],
-    SCHEMA_URL,
-  )
+  Resource::builder()
+    .with_service_name(service_name)
+    .with_attribute(KeyValue::new(
+      SERVICE_VERSION,
+      env!("CARGO_PKG_VERSION"),
+    ))
+    .build()
 }
 
 pub fn tracer(endpoint: &str, service_name: String) -> Tracer {
-  let provider = opentelemetry_sdk::trace::TracerProvider::builder()
-    .with_resource(resource(service_name.clone()))
-    .with_sampler(Sampler::AlwaysOn)
-    .with_batch_exporter(
-      opentelemetry_otlp::SpanExporter::builder()
-        .with_tonic()
-        .with_endpoint(endpoint)
-        .with_timeout(Duration::from_secs(3))
-        .build()
-        .unwrap(),
-      opentelemetry_sdk::runtime::Tokio,
-    )
-    .build();
+  let provider =
+    opentelemetry_sdk::trace::TracerProviderBuilder::default()
+      .with_resource(resource(service_name.clone()))
+      .with_sampler(Sampler::AlwaysOn)
+      .with_batch_exporter(
+        opentelemetry_otlp::SpanExporter::builder()
+          .with_http()
+          .with_endpoint(endpoint)
+          .with_timeout(Duration::from_secs(3))
+          .build()
+          .unwrap(),
+      )
+      .build();
   global::set_tracer_provider(provider.clone());
   provider.tracer(service_name)
 }
