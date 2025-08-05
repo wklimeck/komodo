aa9998268786bfa8b87be14fd127e6ed3f11ee9b
mbecker20 <becker.maxh@gmail.com>
2025-08-05 01:16:56 -0400

    dev-13 logs

--- bin/cli/src/command/database.rs
+++ bin/cli/src/command/database.rs
@@ -5,15 +5,17 @@ use crate::config::cli_config;
 pub async fn backup() -> anyhow::Result<()> {
   let config = cli_config();
 
+  println!("");
   println!(
-    "🦎 {} database {} utility 🦎",
+    "🦎 {} Database {} Utility 🦎",
     "Komodo".bold(),
-    "backup".green()
+    "Backup".green().bold()
   );
   println!(
     "{}",
     "Backup all database contents to gzip compressed files.".dimmed()
   );
+  println!("");
   println!("{}: {:?}", "Root Folder".dimmed(), config.backup_folder);
 
   crate::command::wait_for_enter("start backup")?;
@@ -25,6 +27,24 @@ pub async fn backup() -> anyhow::Result<()> {
 
 pub async fn restore() -> anyhow::Result<()> {
   let config = cli_config();
+
+  println!("");
+  println!(
+    "🦎 {} Database {} Utility 🦎",
+    "Komodo".bold(),
+    "Restore".red().bold()
+  );
+  println!(
+    "{}",
+    "Restores database contents from gzip compressed files.".dimmed()
+  );
+  println!("");
+  println!("{}: {:?}", "Root Folder".dimmed(), config.backup_folder);
+  if let Some(restore_folder) = &config.restore_folder {
+    println!("{restore_folder:?}: {:?}", "Restore Folder".dimmed());
+  }
+
+  crate::command::wait_for_enter("start restore")?;
 
   // Initialize the whole client to ensure the target database is indexed.
   let db = database::Client::new(&config.database).await?;
@@ -39,6 +59,19 @@ pub async fn restore() -> anyhow::Result<()> {
 
 pub async fn copy() -> anyhow::Result<()> {
   let config = cli_config();
+
+  println!("");
+  println!(
+    "🦎 {} Database {} Utility 🦎",
+    "Komodo".bold(),
+    "Copy".blue().bold()
+  );
+  println!(
+    "{}",
+    "Copies database contents to another database.".dimmed()
+  );
+
+  crate::command::wait_for_enter("start copy")?;
 
   let source_db = database::init(&config.database).await?;
   // Initialize the full client to perform indexing
